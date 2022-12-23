218eb97c069be5323a36b81f530bec959234fdee
Sebastian Jingoi <sebi.jingoi@gmail.com>
2022-12-23 14:14:43 -0500

    made seperate ai file added checker to setkey bot now uses api key set in database rather than local api key

---
+++ bot/ai.py
@@ -0,0 +1,35 @@
+import openai
+import database
+
+AI_ERROR_MESSAGE = 'An internal error has occured. Please try again in a few moments.'
+AI_INVALID_KEY_MESSAGE = 'No valid API key provided. To set an API key, use the "setkey" command in a private channel. You can find your API key at https://beta.openai.com/account/api-keys.'
+TEST_CASE = 'What is 1+1?'
+
+async def get_ai_response(input_txt: str, api_key:str) -> str:
+    openai.api_key = api_key
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        max_tokens=1000,
+        prompt=input_txt,
+        temperature=0.5
+    )
+
+    return response['choices'][0]['text']
+
+async def query_ai(prompt: str, server_id: int) -> str:
+    api_key = database.get_ai_key(server_id)
+    try:
+        ai_response = await get_ai_response(prompt, api_key)
+    except openai.error.AuthenticationError:
+        ai_response = AI_INVALID_KEY_MESSAGE
+    except:
+        ai_response = AI_ERROR_MESSAGE
+    return ai_response
+
+async def is_valid_key(key: str) -> bool:
+    try:
+        await get_ai_response(TEST_CASE, key)
+    except:
+        return False
+    return True
--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,16 @@
-import openai
+
 import database
 from dotenv import load_dotenv, find_dotenv
 import os
 import discord
+import ai
 
 env_dir = find_dotenv('keys.env', True)
 load_dotenv(env_dir)
 
-openai.api_key = os.environ["OPEN_AI_KEY"]
 DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
-AI_ERROR_MESSAGE = 'An internal error has occured. Please try again in a few moments.'
+VALID_API_KEY = 'Key validated and set.'
+INVALID_API_KEY = 'The key provided is invalid. You can find your API key at https://beta.openai.com/account/api-keys.'
 
 
 intents = discord.Intents.default()
@@ -18,17 +19,6 @@ intents.messages = True
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
-async def get_ai_response(input_txt: str) -> str:
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        max_tokens=1000,
-        prompt=input_txt,
-        temperature=0.5
-    )
-
-    return response['choices'][0]['text']
-
 
 @client.event
 async def on_message(message):
@@ -48,27 +38,31 @@ async def on_message(message):
 async def execute_cmd(command: str, message = None):
     server_id = message.guild.id
     match command.split():
+
+        # AI COMMAND
         case ["ai", *args]:
             prompt = lst_to_string(args)
-            ai_response = await query_ai(prompt)
+            ai_response = await ai.query_ai(prompt, server_id)
             for section in range(0, len(ai_response), 2000):
                 await message.reply(ai_response[section:section + 2000])
+
+        # INITIALIZE SERVER
         case ["init"]:
             server_name = message.guild.name
             database.add_server(server_id, server_name)
+
+        # SET COMMAND PREFIX
         case ["setprefix", prefix]:
             if len(prefix) == 1:
                 database.update_prefix(server_id, prefix)
+
+        # SET OPENAI API KEY
         case ["setkey", key]:
-            database.update_openai_key(server_id, key)
-
-
-async def query_ai(prompt: str) -> str:
-    try:
-        ai_response = await get_ai_response(prompt)
-    except:
-        ai_response = AI_ERROR_MESSAGE
-    return ai_response
+            if await ai.is_valid_key(key):
+                database.update_openai_key(server_id, key)
+                await message.reply(VALID_API_KEY)
+            else:
+                await message.reply(INVALID_API_KEY)
 
 
 def lst_to_string(lst: list[str]) -> str:
