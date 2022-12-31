0e8fb558dd82829adf7fd6d43706b9c298ad7873
Sebastian Jingoi <sebi.jingoi@gmail.com>
2022-12-31 15:19:20 -0500

    Database connection now opens and closes when needed, instead of always being open.

--- bot/database.py
+++ bot/database.py
@@ -10,21 +10,24 @@ DATABASE = os.environ["DB_NAME"]
 USERNAME = os.environ["DB_USER"]
 PASSWORD = os.environ["DB_PASSWORD"]
 
-
-mydb = mysql.connector.connect(
-    host = HOST,
-    user = USERNAME,
-    password = PASSWORD,
-    database = DATABASE
-)
-
+def get_connection():
+    mydb = mysql.connector.connect(
+        host = HOST,
+        user = USERNAME,
+        password = PASSWORD,
+        database = DATABASE
+    )
+    return mydb
 
 def add_server(id: int, name: str, cmd_prefix: str = '$', openai_key: str = None) -> None:
+    mydb = get_connection()
     dbcursor = mydb.cursor()
     sql = "INSERT INTO dservers (server_id, server_name, cmd_prefix, openai_key) VALUES (%s, %s, %s, %s)"
     val = (id, name, cmd_prefix, openai_key)
     dbcursor.execute(sql, val)
     mydb.commit()
+    dbcursor.close()
+    mydb.close()
 
 
 def get_ai_key(server_id: int):
@@ -36,10 +39,13 @@ def get_cmd_prefix(server_id: int):
 
 
 def get_from_dserver_table(server_id: int, collumn: str):
+    mydb = get_connection()
     dbcursor = mydb.cursor()
     dbcursor.execute("SELECT " + collumn + " FROM dservers WHERE server_id = " + str(server_id))
     for key in dbcursor:
         return key[0]
+    dbcursor.close()
+    mydb.close()
 
 
 def update_openai_key(server_id: int, key: int):
@@ -51,9 +57,12 @@ def update_prefix(server_id: int, prefix: str):
 
 
 def update_dserver(server_id: int, collumn: str, value: str):
+    mydb = get_connection()
     dbcursor = mydb.cursor()
     dbcursor.execute("UPDATE dservers SET " + collumn + " = '" + value + "' WHERE server_id = " + str(server_id))
     mydb.commit()
+    dbcursor.close()
+    mydb.close()
 
 
 def server_in_database(server_id: int) -> bool:
