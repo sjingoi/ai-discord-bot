0e7da68683258e6670ea078ae161cb383687d249
Chris Mikhail <samuelmikhc@gmail.com>
2023-01-17 12:43:37 -0500

    commit -m "Added contact and Tutorial pages"

--- website/script.js
+++ website/script.js
@@ -2,24 +2,24 @@ function respondNav() {
 
   WindowWidth = $(window).width();
 
-  if (WindowWidth <= 469) {
+  if (WindowWidth <= 440) {
     $(".navbar-links").hide();
-    $("ul").css("display", "block");
-    $("ul").css("float", "none");
-    $("ul").css("background-color", "rgb(30 32 40)");
+    $(".navbar ul").css("display", "block");
+    $(".navbar ul").css("float", "none");
+    $(".navbar ul").css("background-color", "rgb(30 32 40)");
     $(".navbar-links li").css("padding", "1rem");
-    $("ul").css("transform", "translate(0, 0)");
+    $(".navbar ul").css("transform", "translate(0, 0)");
     $(".toggle-button").show();
   } else {
-    $("ul").css("display", "flex");
-    $("ul").css("float", "right");
+    $(".navbar ul").css("display", "flex");
+    $(".navbar ul").css("float", "right");
     $(".navbar-links li").css("padding", "0.5rem");
     $(".navbar").css("padding", "1rem");
     $(".GPTBot").css("padding", "0");
     $(".navbar-links li").css("border-bottom", "0");
     $(".navbar-links").css("border-top", "0");
-    $("ul").css("background-color", "rgb(39 41 52)");
-    $("ul").css("transform", "translate(0, -1.8rem)");
+    $(".navbar ul").css("background-color", "rgb(39 41 52)");
+    $(".navbar ul").css("transform", "translate(0, -1.8rem)");
     $(".navbar-links").show();
     $(".toggle-button").hide();
   }
@@ -34,11 +34,26 @@ $(window).resize(function () {
 })
 
 $(".toggle-button").click(function () {
-  $("ul li").css("display", "block");
-  $("ul li").css("text-align", "right");
+  $(".navbar ul li").css("display", "block");
+  $(".navbar ul li").css("text-align", "right");
   $(".navbar").css("padding", "0");
   $(".navbar-links li").css("border-bottom", "2px solid #777");
   $(".navbar-links").css("border-top", "2px solid #777");
   $(".GPTBot").css("padding", "1rem");
   $(".navbar-links").slideToggle("fast");
 });
+
+var typed = new Typed(".typed", {
+  strings: ["What should I do today?", "What happened on June 4, 1989?", "What is the derivative of sin(x)?", "How to bake a cake?",
+    "What country has the highest life expectancy?", "How many moons does Jupiter have?", "Where is mount Everest located?",
+    "Which American president is on the $5 bill?", "Tell me a bad joke.", "Give me a good workout routine.", "What is a group of pandas called?",
+    "What came first, the chicken or the egg?", "What movie should I watch?", "Prove that lim(2x+3) = -3", "What is a nibble?", "What are the top 10 movies of all time?",
+    "How to rotate a quaternion?", "What are the first 8 digits of pi?", "Prove the Caley Hamilton theory.", "Write a song about dogs."],
+  typeSpeed: 70,
+  backSpeed: 10,
+  shuffle: true,
+  loop: true
+})
+
+
+
